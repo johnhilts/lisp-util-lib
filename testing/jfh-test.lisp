f7e067a001dfcd06867e3c0873215d441be4d377
John Hilts <johnhilts@live.com>
2020-09-17 02:21:10 -0700

    utils - support multiple tests

--- testing/jfh-test.lisp
+++ testing/jfh-test.lisp
@@ -4,17 +4,20 @@
 (in-package #:jfh-test)
 
 (defmacro test-spec (key text &body form)
-                                        ; (print (length form))
   (labels
       ((test-r (key text form)
+         (format t "~&FORM: ~a~%" form)
          (list
           (when (or (equal :description key) (equal :category key) (equal :it key))
             `(format t "~&~a~%" ,text))
 
           (when (listp form)
             (if (equal (car form) 'test-spec)
-                `(progn ,@(test-r (cadr form) (caddr form) (cadddr form)))
-                `(format t "~&The form: ~a ~a!" ',form (if (eval ,form) "passes" "fails")))))))
+                `(progn
+                   ,@(test-r (cadr form) (caddr form) ())
+                   ,@(mapcar #'(lambda (form) `(progn ,@(test-r (cadr form) (caddr form) (cadddr form)))) (nthcdr 3 form)))
+                (unless (null form)
+                  `(format t "~&The form: ~a ~a!" ',form (if (eval ,form) "passes" "fails"))))))))
     `(progn ,@(test-r key text (car form)))))
 
 (defun an-example ()
@@ -23,4 +26,6 @@
       (test-spec :it "should add together 2 numbers"
         (= 5 (+ 2 3)))
       (test-spec :it "should add together 2 other numbers"
-        (= 10 (+ 6 4))))))
+        (= 10 (+ 6 4)))
+      (test-spec :it "should add together 3 numbers!"
+        (= 12 (+ 6 2 4))))))
